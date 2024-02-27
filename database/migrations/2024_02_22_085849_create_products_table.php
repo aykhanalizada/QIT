fa711002462a962a67a2707202bf953c92be546a
aykhanalizada <ayxan.alizade89@gmail.com>
2024-02-27 10:48:26 +0400

    aaaa

--- database/migrations/2024_02_22_085849_create_products_table.php
+++ database/migrations/2024_02_22_085849_create_products_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            //category
-
+            $table->foreignId('category')->constrained();
+            $table->foreignId('country')->constrained();
+            $table->foreignId('measure')->constrained();
             $table->timestamps();
         });
     }
